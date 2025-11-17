6b6d10b37f6e2caf6ad271eccbe7e8ec8d248644
Nanda998 <nandasankar2004@gmail.com>
2025-11-17 10:18:30 +0530

    msg

---
+++ demo1/src/App.jsx
@@ -0,0 +1,14 @@
+import AkarshOne from "./akarsh/akarsh"
+function App(){
+  return(
+    <>
+    <h1> hidoihweoidhe</h1>
+    <AkarshOne/>
+    <p>csgcuydwcuyew</p>
+    <AkarshOne/>
+    </>
+      
+  )
+
+}
+export default App
---
+++ demo1/src/akarsh/akarsh.jsx
@@ -0,0 +1,9 @@
+function AkarshOne(){
+    return(
+        <>
+        <h1>Akarsh Mature function</h1>
+        </>
+    )
+
+}
+export default  AkarshOne
